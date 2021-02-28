947ccf96a70cc79d61e2a299efa87ce41a784cdd
mercy021 <kinyanjuimercywangari@gmail.com>
2021-02-28 11:01:46 +0300

    first commit

---
+++ src/Function assignment.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var name:String= "Hello Mercy"
+    println(name)
+
+    var modulus=(20%10)
+    var result="num1,num2"
+    println(modulus)
+
+    var addition=(30+20+10+40)
+    var reSult="no1,no2,no3,no4"
+    println(addition)
+
+    var tall:Boolean=true
+    println(tall)
+
+
+
+
+}
+fun modulus(num1:Int,num2:Int):Int {
+    var modulus=num1%num2
+    return(modulus)
+
+
+}
+fun addition(no1:Int,no2:Int,no3:Int,no4:Int):Int{
+    var addition=no1+no2+no3+no4
+    return(addition)
+
+}
